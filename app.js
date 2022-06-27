e26bd26a5e197ac8ca752392cc3897262246e017
Flame-Y <94735231+Flame-Y@users.noreply.github.com>
2022-06-27 22:05:54 +0800

    Add files via upload
    将MongoDB本地服务器换成了云服务器

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ app.use(cors());
 const account = require("./router/account");
 
 app.use("/", account);
-
+const connection =
+  "mongodb+srv://admin:114514@cluster0.imyvizw.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(
-  "mongodb://localhost:27017/myAccount",
-  { useNewUrlParser: true },
-  function (err) {
-    if (err) {
-      console.log("数据库连接失败");
-    } else {
-      console.log("数据库连接成功");
-      // 监听http请求
-      app.listen(27017, () => {
-        console.log("app listening on port 27017.");
-      });
-    }
+  connection,
+  {
+    useNewUrlParser: true,
+  },
+  () => {
+    console.log("mongoose连接成功了！");
+    app.listen(27017, () => {
+      console.log("app listening on port 27017.");
+    });
   }
 );
+mongoose.connection.on("error", console.error);
