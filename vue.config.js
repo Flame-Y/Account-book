c93ab28f3e6ff7cf7b296a69facbfd12d41ccf7c
Flame-Y <94735231+Flame-Y@users.noreply.github.com>
2022-06-27 18:49:21 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,14 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-
+  publicPath: "./",
   devServer: {
     proxy: {
       "/api": {
-        target: "http://119.23.54.229:8080/qds/account",
+        target: "http://localhost:27017/account",
         pathRewrite: {
-          '^/api': '' // 将开头的 '/api' 替换成空字符串（^ 是开头的意思，'^/api' 表示以 /api 开头）
+          "^/api": "", // 将开头的 '/api' 替换成空字符串（^ 是开头的意思，'^/api' 表示以 /api 开头）
         },
-        ws: true,
         changeOrigin: true,
       },
     },
